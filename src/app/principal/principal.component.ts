cf7e39b8ff88beef3e59f6d4d0f58bad5fac2fc4
Barbara Hellen <barbara.pereira@modalgr.com.br>
2021-08-19 12:11:22 -0300

    calculadora finalizada

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -7,14 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrincipalComponent implements OnInit {
 
-  valorTotal = 0;
+  valorTotal: string = '';
+  valorAtual: string = '';
+  valorAnterior: string = '';
   constructor() { }
 
   ngOnInit() {
   }
 
-  addOnInput(val: number){
-    this.valorTotal += val;
+  addOnInput(val: string){
+    let antes = this.valorAtual;
+    let atual = val;
+    let pegaUltimo = antes.slice(-1);
+    if((pegaUltimo == '+' && atual == '+')){
+      return;
+    }
+    if((pegaUltimo == '-' && atual == '-')){
+      return;
+    }
+    if((pegaUltimo == '/' && atual == '/')){
+      return;
+    }
+    this.valorAtual += val;
+      switch(val)
+      {
+        case '=':
+        {
+          this.valorTotal = this.valorAtual.substring(0, this.valorAtual.length -1);
+          this.valorTotal = eval(this.valorTotal)
+          this.valorAnterior = this.valorTotal
+          this.valorAtual = '';
+        };
+        case 'ac':
+        {
+          this.valorAtual = '';
+        }
+      } 
   }
+
+  // sum(mais){
+  //   this.valorAtual =+ mais
+  // }
+
+  
 
 }
